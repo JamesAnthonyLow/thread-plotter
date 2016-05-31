3d6c3101133767fb9018a371f0fe6ed1da8184f4
JamesAnthonyLow <jamesalowenthal@gmail.com>
2016-05-31 14:56:00 -0400

    add colors

--- key.rb
+++ key.rb
@@ -4,8 +4,6 @@ printf("-"*106+"\n")
 printf("|%-20s|%-20s|%-20s|%-20s|%-20s|\n", "Task Number", "Task Name", "Period", "Compute Time", "Priority")
 printf("-"*106+"\n")
 TASKS.each_with_index do |t, i|
-  printf("|%-20s|%-20s|%-20s|%-20s|%-20s|\n", i, t.name, t.period, t.compute_time, t.priority)
+  printf("|%-20s|%-20s|%-20s|%-20s|%-20s|\n", "\033\[0;36m"+i.to_s+"\x1b[0m", t.name, t.period, t.compute_time, t.priority)
   printf("-"*106+"\n")
 end
-
-
